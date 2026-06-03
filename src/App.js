802371283dbffa5c91ba9e8daa38864ad07720b6
ghaliattiamoussa <ghalyattiy@gmail.com>
2026-06-03 09:14:39 +0300

    all 195 countries states and provinces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { citiesData, statesData } from './citiesData';
+import { citiesData, statesData } from './statesData';
 
 function App() {
   const [countries, setCountries] = useState([]);
